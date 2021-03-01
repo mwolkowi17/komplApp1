cce9c497f52a56c39c1ec90313483ecbdd4ab0b3
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-03-01 21:27:10 +0100

    icon clicked not ready

--- src/icons.js
+++ src/icons.js
@@ -1,8 +1,8 @@
 export const ikonyglowne = {
     first: "./images/hejt_cz1.png",
-    firtscliced: "./images/hejt_cz1.png",
+    firtscliced: "./images/hejt_cz1clicked.png",
     second: "./images/hejt_cz2.png",
-    secondcliced: "./images/hejt_cz2.png",
+    secondcliced: "./images/hejt_cz2clicked.png",
     third: "./images/hejt_cz3.png",
-    thirdcliced: "./images/hejt_cz3.png"
+    thirdcliced: "./images/hejt_cz3clicked.png"
 }
