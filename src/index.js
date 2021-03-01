6364e59d97c1e754715978753b84ed395685f052
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-03-01 16:51:19 +0100

    vue connected

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue/dist/vue.js';
+
+var app = new Vue({
+    el: '#app',
+    data: () => {
+        return{
+            message:"hello vue"
+        }
+    },
+    methods: {
+    }
+})
