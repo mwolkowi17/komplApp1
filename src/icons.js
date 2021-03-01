df46bd28aff916a5a4a949ad40ea26f96db76ac7
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-03-01 17:18:19 +0100

    main icons added

---
+++ src/icons.js
@@ -0,0 +1,8 @@
+export const ikonyglowne = {
+    first: "./images/hejt_cz1.png",
+    firtscliced: "./images/hejt_cz1.png",
+    second: "./images/hejt_cz2.png",
+    secondcliced: "./images/hejt_cz2.png",
+    third: "./images/hejt_cz3.png",
+    thirdcliced: "./images/hejt_cz3.png"
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import Vue from 'vue/dist/vue.js';
+import { backgrounds } from './backgroundData.js';
+import { ikonyglowne } from './icons.js';
 
 var app = new Vue({
     el: '#app',
     data: () => {
         return{
-            message:"hello vue"
+            message:"hello vue",
+            appbackground: {
+                src: backgrounds[0]
+            },
+            ikon1: {
+                src: ikonyglowne.first,
+                src2: ikonyglowne.firtscliced,
+                seen: true,
+                hover: false
+              },
+              ikon2: {
+                src: ikonyglowne.second,
+                src2: ikonyglowne.secondcliced,
+                seen: true,
+                hover: false
+              },
+              ikon3: {
+                src: ikonyglowne.third,
+                src2: ikonyglowne.thirdcliced,
+                seen: true,
+                hover: false
+              },
         }
     },
     methods: {
