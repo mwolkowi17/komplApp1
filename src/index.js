80e4edf41d149c8c5d0898e4188a40b62f0644d0
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-03-03 08:48:03 +0100

    first part nearly ready

--- src/index.js
+++ src/index.js
@@ -4,82 +4,109 @@ import { ikonyglowne } from './icons.js';
 import { planszeA } from './planszeAdane.js';
 
 var app = new Vue({
-    el: '#app',
-    data: () => {
-        return{
-            message:"hello vue",
-            appbackground: {
-                src: backgrounds[0]
-            },
-            ikon1: {
-                src: ikonyglowne.first,
-                src2: ikonyglowne.firtscliced,
-                seen: true,
-                hover: false
-              },
-              ikon2: {
-                src: ikonyglowne.second,
-                src2: ikonyglowne.secondcliced,
-                seen: true,
-                hover: false
-              },
-              ikon3: {
-                src: ikonyglowne.third,
-                src2: ikonyglowne.thirdcliced,
-                seen: true,
-                hover: false
-              },
-              video: {
+  el: '#app',
+  data: () => {
+    return {
+      message: "hello vue",
+      appbackground: {
+        src: backgrounds[0]
+      },
+      ikon1: {
+        src: ikonyglowne.first,
+        src2: ikonyglowne.firtscliced,
+        seen: true,
+        hover: false
+      },
+      ikon2: {
+        src: ikonyglowne.second,
+        src2: ikonyglowne.secondcliced,
+        seen: true,
+        hover: false
+      },
+      ikon3: {
+        src: ikonyglowne.third,
+        src2: ikonyglowne.thirdcliced,
+        seen: true,
+        hover: false
+      },
+      video: {
 
-                src: "",
-                seen: false
-              },
-              buttonforward1:{
-                seen: false
-              },
-              buttonreverse1:{
-                seen: false
-              },
-              buttonforward2:{
-                seen: false
-              },
-              buttonreverse2:{
-                seen: false
-              },
-              buttonforward3:{
-                seen: false
-              },
-              buttonreverse3:{
-                seen: false
-              }
-        }
+        src: "",
+        seen: false
+      },
+      buttonforward1: {
+        seen: false
+      },
+      buttonreverse1: {
+        seen: false
+      },
+      buttonforward2: {
+        seen: false
+      },
+      buttonreverse2: {
+        seen: false
+      },
+      buttonforward3: {
+        seen: false
+      },
+      buttonreverse3: {
+        seen: false
+      },
+      licznik: {
+        planszeAlicz: 0,
+        planszeBlicz: 0,
+        planszeClicz: 0
+      }
+    }
+  },
+  methods: {
+    ikon1change1: function () {
+      this.ikon1.hover = false;
+      this.appbackground.src = planszeA[0];
+      this.ikon2.seen = false;
+      this.ikon3.seen = false;
+      this.buttonreverse1.seen = true;
+      this.buttonforward1.seen = true;
     },
-    methods: {
-      ikon1change1: function(){
-        this.ikon1.hover = false;
-        this.appbackground.src = planszeA[0];
-        this.ikon2.seen = false;
-        this.ikon3.seen = false;
-        this.buttonreverse3.seen = true;
-        this.buttonforward3.seen = true;
-      },
-      ikon2change1: function(){
-        this.ikon2.hover = false;
-        this.appbackground.src = backgrounds[1];
-        this.ikon1.seen = false;
-        this.ikon3.seen = false;
-        this.video.seen = true;
-        this.buttonreverse2.seen = true;
-        this.buttonforward2.seen = true;
-      },
-      ikon3change1: function(){
-        this.ikon3.hover = false;
+    ikon2change1: function () {
+      this.ikon2.hover = false;
+      this.appbackground.src = backgrounds[1];
+      this.ikon1.seen = false;
+      this.ikon3.seen = false;
+      this.video.seen = true;
+      this.buttonreverse2.seen = true;
+      this.buttonforward2.seen = true;
+    },
+    ikon3change1: function () {
+      this.ikon3.hover = false;
       this.appbackground.src = backgrounds[1];
       this.ikon1.seen = false;
       this.ikon2.seen = false;
       this.video.seen = true;
       this.buttonreverse3.seen = true;
-      this.buttonforward3.seen = true;      
+      this.buttonforward3.seen = true;
+    },
+    btnforA: function(){
+      this.licznik.planszeAlicz++;
+      this.appbackground.src = planszeA[this.licznik.planszeAlicz];
+      if(this.licznik.planszeAlicz === 8){
+        this.appbackground.src = backgrounds[1];
+        this.video.seen = true;
       }
-        }
+      if(this.licznik.planszeAlicz === 9){
+        this.licznik.planszeAlicz = 0;
+        this.video.seen=false;
+        this.appbackground.src = backgrounds[0];
+        this.buttonreverse1.seen = false;
+        this.buttonforward1.seen = false;
+        this.ikon1.seen = true;
+        this.ikon2.seen = true;
+        this.ikon3.seen = true;
+
+      }
+    },
+    btnrevA: function(){
+
+    }
+  }
 })
